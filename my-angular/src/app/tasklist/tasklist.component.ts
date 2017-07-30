ee1c43fb9b817ebea219d3d9e4406136563d3b4d
Lung Lao <eddielao@gmail.com>
2017-07-29 17:52:06 -0700

    table mockup

---
+++ my-angular/src/app/tasklist/tasklist.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tasklist',
+  templateUrl: './tasklist.component.html',
+  styleUrls: ['./tasklist.component.css']
+})
+
+export class TaskListComponent {
+
+}
