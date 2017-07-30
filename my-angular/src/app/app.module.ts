3975263c33fde7f8e59a6aecf305059a1c8bfff2
Lung Lao <eddielao@gmail.com>
2017-07-29 18:57:26 -0700

    Rename task list files and folder

--- my-angular/src/app/app.module.ts
+++ my-angular/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { TaskListComponent } from './tasklist/tasklist.component';
+import { TaskListComponent } from './task-list/task-list.component';
 import { TaskDetailComponent } from './task-detail/task-detail.component';
 
 @NgModule({
